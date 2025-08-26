6f689f7d5cace11ac51afdadb412de10b8e4368b
Saad Shaikh <shaikhsaad256@gmail.com>
2025-08-26 20:40:19 +0530

    Problem-14

---
+++ 69-Sqrt.js
@@ -0,0 +1,7 @@
+// Define a function named mySqrt that takes a number x
+var mySqrt = function (x) {
+  // Step 1: Find the square root of x using Math.sqrt
+  // Step 2: Use Math.floor to round it down to the nearest integer
+  // Step 3: Return the floored value
+  return Math.floor(Math.sqrt(x));
+};
